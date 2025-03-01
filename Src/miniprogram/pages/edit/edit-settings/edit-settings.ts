fb4029b6eae63d1752a1844432fdac111612f663
Choihyobin111 <454974197@qq.com>
2025-03-02 02:49:26 +0800

    主页-编辑行程解耦

---
+++ Src/miniprogram/pages/edit/edit-settings/edit-settings.ts
@@ -0,0 +1,232 @@
+import { currencyList, timezoneList, Tour, Transportation } from '../../../utils/tour';
+import { MILLISECONDS, formatDate, formatTime, } from '../../../utils/util';
+
+const app = getApp<IAppOption>();
+
+
+Component({
+    data: {
+           currencyList: currencyList,
+           timezoneList: timezoneList,
+          
+           currentStartDateStr: '',
+           currentEndDateStr: '',
+           currentTimezoneStr: '',
+   
+           // 数据缓存
+           currentTour: null as Tour | null,
+           selectingTimeOffset: -480,
+       },
+       lifetimes: {
+        created() {
+
+        },
+        attached() {
+            console.log("加载行程默认设置...");
+                    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
+                        const page: any = getCurrentPages().pop();
+                        this.getTabBar().setData({ value: '/' + page.route })
+                    }
+                    this.setData({
+                        selectingTour: app.globalData.selectingTour,
+                        currentTour: app.globalData.currentTour,
+                    });
+                    if (app.globalData.currentTour) {
+                        const currentTour = app.globalData.currentTour as Tour;
+                        this.setData({
+                            currentStartDateStr: formatDate(currentTour.startDate, currentTour.timeOffset),
+                            currentEndDateStr: formatDate(currentTour.endDate, currentTour.timeOffset),
+                            currentTimezoneStr: timezoneList.find(tz => tz.value === currentTour.timeOffset)?.label || '未知时区',
+                            currentDateRange: [
+                                new Date(currentTour.startDate).getTime(),
+                                new Date(currentTour.endDate).getTime() + MILLISECONDS.DAY - MILLISECONDS.MINUTE
+                            ],
+                            currentStartDateStrList: currentTour.locations.map(location => formatTime(
+                                currentTour.startDate + location.startOffset, location.timeOffset
+                            )),
+                            currentEndDateStrList: currentTour.locations.map(location => formatTime(
+                                currentTour.startDate + location.endOffset, location.timeOffset
+                            )),
+                            currentTimezoneStrList: currentTour.locations.map(location => {
+                                const timezone = timezoneList.find(tz => tz.value === location.timeOffset);
+                                return timezone ? timezone.label : '未知时区'
+                            }),
+                            currentDurationStrList: currentTour.transportations.map(transportation => {
+                                return new Transportation(transportation).getDurationString();
+                            })
+                        });
+                    }
+                    else {
+                        this.setData({ currentDateRange: null });
+                    }
+                    console.log("加载完毕！")
+        },
+        moved() {
+    
+        },
+        detached() {
+    
+        },
+      },
+    methods:{
+            getLatestTour(){
+                       //使用最新数据创建实例
+                       if (!this.data.currentTour) return;
+                       const latestTour = app.globalData.currentTour || this.data.currentTour;
+                       const currentTour = new Tour(latestTour);
+                       return currentTour;
+                   },
+            /**
+             * 行程整体编辑
+             */
+            handleTitleInput(e: any) {
+                if (!this.data.currentTour) return;
+
+                const currentTour = this.getLatestTour();
+                if(!currentTour) return;
+
+                currentTour.title = e.detail.value;
+                app.globalData.currentTour = currentTour;
+                app.globalData.tourList = app.globalData.tourList.map((tour: any) => {
+                    if (tour.id === currentTour.id) {
+                        tour.title = currentTour.title;
+                    }
+                    return tour;
+                });
+                this.setData({ currentTour: currentTour });
+                wx.setStorageSync('tour-' + currentTour.id, currentTour);
+            },
+            exchangeTourCurrency() {
+                if (!this.data.currentTour) return;
+
+                const currentTour = this.getLatestTour();
+                if(!currentTour) return;
+
+                const newMainCurrency = currentTour.subCurrency;
+                const newSubCurrency = currentTour.mainCurrency;
+                currentTour.mainCurrency = newMainCurrency;
+                currentTour.subCurrency = newSubCurrency;
+                currentTour.currencyExchangeRate = Number((1 / currentTour.currencyExchangeRate).toFixed(7));
+
+                app.globalData.currentTour = currentTour;
+                this.setData({ currentTour: currentTour });
+                wx.setStorageSync('tour-' + currentTour.id, currentTour);
+            },
+            async getCurrencyExchangeRate() {
+                if (!this.data.currentTour) return;
+
+               const currentTour = this.getLatestTour();
+                if(!currentTour) return;
+
+                await currentTour.getExchangeRate();
+
+                app.globalData.currentTour = currentTour;
+                this.setData({ currentTour: currentTour });
+                wx.setStorageSync('tour-' + currentTour.id, currentTour);
+            },
+            onTourCurrencyExchangeRateInput(e: any) {
+                if (!this.data.currentTour) return;
+
+                const currentTour = this.getLatestTour();
+                if(!currentTour) return;
+
+                const { priceError } = this.data;
+                const isNumber = /^\d+(\.\d+)?$/.test(e.detail.value);
+                if (priceError === isNumber) {
+                    this.setData({
+                        priceError: !isNumber,
+                    });
+                }
+                if (!isNumber) return;
+                currentTour.currencyExchangeRate = Number(e.detail.value);
+                app.globalData.currentTour = currentTour;
+                this.setData({ currentTour: currentTour });
+                wx.setStorageSync('tour-' + currentTour.id, currentTour);
+            },
+            handleCalendar() {
+                this.setData({ calendarVisible: true });
+            },
+            handleCalendarConfirm(e: any) {
+                if (!this.data.currentTour) return;
+
+                const currentTour = this.getLatestTour();
+                if(!currentTour) return;
+
+                currentTour.startDate = new Date(e.detail.value[0]).getTime();
+                currentTour.endDate = new Date(e.detail.value[1]).getTime();
+                this.setData({
+                    currentDateRange: [
+                        new Date(currentTour.startDate).getTime(),
+                        new Date(currentTour.endDate).getTime() + MILLISECONDS.DAY - MILLISECONDS.MINUTE
+                    ]
+                });
+
+                app.globalData.currentTour = currentTour;
+                app.globalData.tourList = app.globalData.tourList.map((tour: any) => {
+                    if (tour.id === currentTour.id) {
+                        tour.startDate = formatDate(currentTour.startDate, currentTour.timeOffset);
+                        tour.endDate = formatDate(currentTour.endDate, currentTour.timeOffset);
+                    }
+                    return tour;
+                });
+                this.setData({
+                    currentTour: currentTour,
+                    currentStartDateStr: formatDate(currentTour.startDate, currentTour.timeOffset),
+                    currentEndDateStr: formatDate(currentTour.endDate, currentTour.timeOffset),
+                    calendarVisible: false
+                });
+                wx.setStorageSync('tour-' + currentTour.id, currentTour);
+            },
+            handleTourTimezoneSelector() {
+                if (!this.data.currentTour) return;
+                this.setData({
+                    selectingTimeOffset: this.data.currentTour.timeOffset,
+                    timezoneVisible: true
+                });
+            },
+            onTimezoneColumnChange(e: any) {
+                this.setData({ selectingTimeOffset: Number(e.detail.value[0]) });
+            },
+            onTimezonePickerCancel() {
+                this.setData({
+                    timezoneVisible: false,
+                    selectingTimeOffset: -480,
+                });
+            },
+            onTourTimezonePickerChange() {
+                if (!this.data.currentTour) return;
+
+                const currentTour = this.getLatestTour();
+                if(!currentTour) return;
+
+                currentTour.timeOffset = this.data.selectingTimeOffset;
+
+                app.globalData.currentTour = currentTour;
+                this.setData({
+                    currentTour: currentTour,
+                    currentTimezoneStr: timezoneList.find(tz => tz.value === currentTour.timeOffset)?.label || '未知时区',
+                    timezoneVisible: false,
+                });
+                wx.setStorageSync('tour-' + currentTour.id, currentTour);
+            },
+            exportTourToClipboard() {
+                if (!this.data.currentTour) return;
+
+                const currentTour = this.getLatestTour();
+                if(!currentTour) return;
+                const exportText = currentTour.toString();
+                wx.setClipboardData({
+                    data: exportText,
+                    success: () => {
+                        wx.showToast({
+                            title: '导出成功',
+                            icon: 'success',
+                            duration: 2000
+                        });
+                    }
+                });
+            },
+
+    },
+
+})
---
+++ Src/miniprogram/pages/index/index-newtour/index-newtour.ts
@@ -0,0 +1,354 @@
+import { Currency, Tour, currencyList } from '../../../utils/tour';
+import { formatDate, MILLISECONDS } from '../../../utils/util';
+
+const app = getApp<IAppOption>();
+
+
+Component({
+  behaviors: [],
+  properties: {
+
+  },
+  data: {
+      creatorArray: [
+      { image: '../../../resources/chika2.png', text: '从零开始创建一个空白行程.', button: '创建', action: 'onCreatorVisibleChange' },
+      { image: '../../../resources/ruby2.png', text: '复制一个已有的行程.', button: '拷贝', action: 'onTourSelectorVisibleChange' },
+      { image: '../../../resources/you2.png', text: '从外部复制的文本数据导入行程.', button: '导入', action: 'onImporterVisibleChange' },
+      ],
+
+      tourList: [] as { id: number; title: string; startDate: string; endDate: string; }[],
+      displayTourList: [] as { label: string; value: number }[],
+      mainCurrencies: currencyList,
+      subCurrencies: currencyList,
+      minDate: new Date(new Date().getTime() - 365 * MILLISECONDS.DAY).getTime(),
+      maxDate: new Date(new Date().getTime() + 365 * MILLISECONDS.DAY).getTime(),
+      currencyText: '主货币:人民币-CNY\n辅货币:日元-JPY',
+
+      creatorVisible: false,
+      importerVisible: false,
+      calendarVisible: false,
+      currencySelectorVisible: false,
+      tourSelectorVisible: false,
+
+      containsTour: false,
+      tourHashMap: new Map(),
+
+      // 创建新行程数据
+      newTourName: '新的旅程',
+      newTourDate: [new Date().getTime(), new Date(new Date().getTime() + MILLISECONDS.DAY).getTime()],
+      newTourStartDateStr: formatDate(new Date()),
+      newTourEndDateStr: formatDate(new Date(new Date().getTime() + MILLISECONDS.DAY)),
+      newTourCurrency: [Currency.CNY, Currency.JPY],
+      newTourTimeOffset: -480,
+
+      // 复制行程数据
+      selectingTourId: 0,
+
+      // 导入行程数据
+      importedTourData: '',
+  },
+  lifetimes: {
+    created() {
+
+    },
+    attached() {
+            this.setData({
+                tourHashMap: new Map<number, string>(),
+                subCurrencies: currencyList.filter(currency => currency.value !== this.data.newTourCurrency[0]),
+            });
+            wx.getStorage({
+                key: 'tourHashMap',
+                success: (res) => {
+                    this.setData({ tourHashMap: new Map(res.data) });
+                    this.updateTourList();
+                },
+            })
+            this.setData({ tourList: app.globalData.tourList });
+            const displayTourList = this.data.tourList.map((tour: any) => {
+                return { label: tour.title, value: tour.id };
+            });
+            this.setData({ displayTourList: displayTourList });
+            },
+    
+    moved() {
+
+    },
+    detached() {
+
+    },
+  },
+  methods: {
+        updateTourHashMap() {
+          return new Promise((resolve) => {
+              wx.getStorage({
+                  key: 'tourHashMap',
+                  success: (res) => {
+                      this.setData({ tourHashMap: new Map(res.data) });
+                      resolve(true);
+                  },
+                  fail: () => {
+                      this.setData({ tourHashMap: new Map() }); // 初始化为空 Map
+                      resolve(true);
+                  },
+              });
+          });
+        },
+        onCreatorVisibleChange() {
+            this.setData({
+                creatorVisible: !this.data.creatorVisible,
+            });
+        },
+        handleCalendar() {
+            this.setData({ calendarVisible: true });
+        },
+        handleCalendarConfirm(e: any) {
+            this.setData({
+                newTourDate: e.detail.value,
+                newTourStartDateStr: formatDate(new Date(e.detail.value[0])),
+                newTourEndDateStr: formatDate(new Date(e.detail.value[1])),
+            });
+        },
+        handleTitleInput(e: any) {
+            this.setData({ newTourName: e.detail.value, });
+        },
+        onCurrencyColumnChange(e: any) {
+            const { column, index } = e.detail;
+            const newTourMainCurrency = currencyList[index].value;
+
+            if (column === 0) {
+                const subCurrencies = currencyList.filter(currency => currency.value !== newTourMainCurrency)
+                this.setData({ subCurrencies: subCurrencies });
+            }
+        },
+        onCurrencyPickerChange(e: any) {
+            const { value, label } = e.detail;
+
+            this.setData({
+                currencySelectorVisible: false,
+                newTourCurrency: value,
+                currencyText: '主货币:' + label[0] + '\n辅货币:' + label[1],
+            });
+        },
+        onCurrencyPickerCancel() {
+            this.setData({
+                currencySelectorVisible: false,
+            });
+        },
+        onCurrencyPicker() {
+            this.setData({ currencySelectorVisible: true });
+        },
+        async createTour() {
+          await this.updateTourHashMap();
+            this.setData({
+                newTourName: this.data.newTourName
+            }, async () => {
+              const tourHashMap = this.data.tourHashMap;
+              const existingIds = Array.from(tourHashMap.keys());
+              const newId = existingIds.length > 0 ? Math.max(...existingIds) + 1 : 0;
+                const newTour = new Tour({
+                    id: newId,
+                    title: this.data.newTourName,
+                    startDate: this.data.newTourDate[0],
+                    endDate: this.data.newTourDate[1],
+                    timeOffset: this.data.newTourTimeOffset,
+                    mainCurrency: this.data.newTourCurrency[0],
+                    subCurrency: this.data.newTourCurrency[1],
+                });
+                await newTour.getExchangeRate();
+                tourHashMap.set(newId, 'tour-' + newTour.id);
+
+                this.setData({
+                    tourHashMap: tourHashMap,
+                    containsTour: true,
+                    creatorVisible: false,
+                });
+
+                Promise.all([
+                    new Promise((resolve, reject) => {
+                        wx.setStorage({
+                            key: 'tour-' + newTour.id,
+                            data: newTour,
+                            success: resolve,
+                            fail: reject,
+                        });
+                    }),
+                    new Promise((resolve, reject) => {
+                        wx.setStorage({
+                            key: 'tourHashMap',
+                            data: Array.from(tourHashMap),
+                            success: resolve,
+                            fail: reject,
+                        });
+                    })
+                ]).then(() => {
+                    this.updateTourList();
+                }).catch((err) => {
+                    console.error('存储 tour 数据时出错: ', err);
+                });
+            });
+        },
+        selectTour(e: any) {
+            const id = e.currentTarget.dataset.index;
+            wx.getStorage({
+                key: 'tour-' + id,
+                success: (res) => {
+                    app.globalData.selectingTour = true;
+                    app.globalData.currentTour = res.data as Tour;
+                },
+            })
+        },
+        async updateTourList() {
+          await this.updateTourHashMap();
+            const tourHashMap = this.data.tourHashMap;
+
+            if (tourHashMap.size == 0) return;
+            const tourList = [] as any[];
+
+            const promises = Array.from(tourHashMap.values()).map(tour_id => {
+                return new Promise((resolve, reject) => {
+                    wx.getStorage({
+                        key: tour_id,
+                        success: (res) => {
+                            const tour = res.data as Tour;
+                            tourList.push({
+                                id: tour.id,
+                                title: tour.title,
+                                startDate: formatDate(tour.startDate),
+                                endDate: formatDate(tour.endDate),
+                            });
+                            resolve(true);
+                        },
+                        fail: (err) => {
+                            reject(err);
+                        }
+                    });
+                });
+            });
+
+            Promise.all(promises).then(() => {
+                const displayTourList = tourList.map((tour: any) => {
+                    return { label: tour.title, value: tour.id };
+                });
+                this.setData({
+                    tourList: tourList,
+                    displayTourList: displayTourList,
+                });
+                app.globalData.tourList = tourList;
+                this.triggerEvent('tourListUpdate', { tourList: tourList });
+            }).catch((err) => {
+                console.error('读取 tour 数据时出错: ', err);
+            });
+        },
+        onTourSelectorVisibleChange() {
+            if (this.data.tourList.length == 0) {
+                wx.showToast({
+                    title: '没有行程',
+                    icon: 'error',
+                    duration: 2000
+                });
+                return;
+            }
+            this.setData({
+                tourSelectorVisible: !this.data.tourSelectorVisible,
+                selectingTourId: this.data.tourList[0].id,
+            });
+        },
+        onTourColumnChange(e: any) {
+            this.setData({ selectingTourId: e.detail.value[0] });
+        },
+        async copyTour() {
+          await this.updateTourHashMap();
+          const tourHashMap = this.data.tourHashMap;
+          const existingIds = Array.from(tourHashMap.keys());
+          const newId = existingIds.length > 0 ? Math.max(...existingIds) + 1 : 0;
+            const selectingTourId = this.data.selectingTourId;
+
+            wx.getStorage({
+                key: 'tour-' + selectingTourId,
+                success: (res) => {
+                    const selectingTour = res.data as Tour;
+                    const newTour = new Tour(selectingTour);
+                    newTour.id = newId;
+
+                    tourHashMap.set(newId, 'tour-' + newId);
+
+                    this.setData({
+                        tourHashMap: tourHashMap,
+                        containsTour: true,
+                    });
+
+                    Promise.all([
+                        new Promise((resolve, reject) => {
+                            wx.setStorage({
+                                key: 'tour-' + newId,
+                                data: newTour,
+                                success: resolve,
+                                fail: reject,
+                            });
+                        }),
+                        new Promise((resolve, reject) => {
+                            wx.setStorage({
+                                key: 'tourHashMap',
+                                data: Array.from(tourHashMap),
+                                success: resolve,
+                                fail: reject,
+                            });
+                        })
+                    ]).then(() => {
+                        this.updateTourList();
+                    }).catch((err) => {
+                        console.error('存储 tour 数据时出错: ', err);
+                    });
+                },
+            });
+        },
+        onImporterVisibleChange() {
+            this.setData({
+                importerVisible: !this.data.importerVisible,
+            });
+        },
+        handleImporterInput(e: any) {
+            this.setData({ importedTourData: e.detail.value });
+        },
+        async importTour() {
+          await this.updateTourHashMap();
+            const tourData = Tour.fromString(this.data.importedTourData);
+            if (tourData instanceof Tour) {
+                const tourHashMap = this.data.tourHashMap;
+                const existingIds = Array.from(tourHashMap.keys());
+                const newId = existingIds.length > 0 ? Math.max(...existingIds) + 1 : 0;
+                tourHashMap.set(newId, 'tour-' + newId);
+                tourData.id = newId;
+
+                this.setData({
+                    tourHashMap: tourHashMap,
+                    containsTour: true,
+                    importerVisible: false,
+                });
+
+                Promise.all([
+                    new Promise((resolve, reject) => {
+                        wx.setStorage({
+                            key: 'tour-' + newId,
+                            data: tourData,
+                            success: resolve,
+                            fail: reject,
+                        });
+                    }),
+                    new Promise((resolve, reject) => {
+                        wx.setStorage({
+                            key: 'tourHashMap',
+                            data: Array.from(tourHashMap),
+                            success: resolve,
+                            fail: reject,
+                        });
+                    })
+                ]).then(() => {
+                    this.updateTourList();
+                }).catch((err) => {
+                    console.error('存储 tour 数据时出错: ', err);
+                });
+            }
+        }
+  },
+});
