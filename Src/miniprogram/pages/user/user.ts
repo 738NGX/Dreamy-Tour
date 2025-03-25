634fac2b9d6f1c5d91dc85720832332d6aaceae2
738NGX <jny738ngx@gmail.com>
2025-03-25 20:55:19 +0800

    用户页面框架

--- Src/miniprogram/pages/user/user.ts
+++ Src/miniprogram/pages/user/user.ts
@@ -1,12 +1,19 @@
+import { UserBasic } from "../../utils/user/user";
+
+const app = getApp<IAppOption>();
+
 Component({
   properties: {
 
   },
   data: {
-
+    currentUser: {} as UserBasic,
   },
   methods: {
     onShow() {
+      this.setData({
+        currentUser: app.currentUser(),
+      })
       if (typeof this.getTabBar === 'function' && this.getTabBar()) {
         const page: any = getCurrentPages().pop();
         this.getTabBar().setData({
