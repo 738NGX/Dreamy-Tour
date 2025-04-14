c6109717da06bae15688ac2dc5dc80bd10c49abd
738NGX <jny738ngx@gmail.com>
2025-04-14 11:36:24 +0800

    信息页布局调整

--- Src/miniprogram/pages/userinfo/userinfo.ts
+++ Src/miniprogram/pages/userinfo/userinfo.ts
@@ -1,6 +1,5 @@
 import { Member } from "../../utils/user/user";
 import { getUserGroupName, userExpTarget, userRoleName } from "../../utils/util";
-import { PostCard } from "../../utils/channel/post";
 import { Channel, ChannelBasic } from "../../utils/channel/channel";
 const app = getApp<IAppOption>();
 
@@ -20,17 +19,6 @@ Component({
     fullChannelList: [] as Channel[],
   },
 
-  lifetimes: {
-    // async ready() {
-    //   const fullChannelList = Array.isArray(this.data.fullChannelList) ? this.data.fullChannelList : [];
-    //   this.setData({
-    //     channelList: fullChannelList.filter(
-    //       channel => channel.name.includes(this.data.searchingValueForChannels || '')
-    //     ),
-    //   });
-    // },
-  },
-
   methods: {
     async onLoad(options: any) {
       const { uid } = options;
@@ -38,7 +26,12 @@ Component({
         selectedUser: await app.getUserDetail(uid),
         currentChannel: await app.loadPublicChannel()
       })
-      console.log("selectedUser",this.data.selectedUser)
+      wx.onThemeChange((res) => {
+        this.setData({
+          isDarkMode: res.theme == 'dark'
+        });
+        console.log('当前主题：', res.theme)
+      });
     },
     copyUid() {
       wx.setClipboardData({
